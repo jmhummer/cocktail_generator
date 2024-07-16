db364efa0ae1816b1003b0daa44dbef109043127
jmhummer <142601033+jmhummer@users.noreply.github.com>
2024-07-16 11:40:25 -0400

    Adding main .js file for function and .ejs for formatting

---
+++ index.js
@@ -0,0 +1,31 @@
+// HINTS:
+// 1. Import express and axios
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+
+// 2. Create an express app and set the port number.
+const app = express();
+const port = 3000;
+const API_URL = "https://www.thecocktaildb.com/api/json/v1/1/";
+
+// 3. Use the public folder for static files.
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// 4. When the user goes to the home page it should render the index.ejs file.
+app.get("/", async (req, res) => {
+    try {
+        const result = await axios.get(API_URL);
+        res.render("index.ejs", { });
+    } catch (error) {
+        // res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+        console.log(error.response.data);
+        res.status(500);
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
